c4998d788e3f74eaea2d0a21d72eaeac4360565b
Jeevasimba <96039278+Jeevasimba@users.noreply.github.com>
2023-05-30 14:01:33 +0530

    Add files via upload

---
+++ EGbook/questiontags.js
@@ -0,0 +1,23 @@
+let count = 0 ;
+
+function ct (x) {
+
+  if(x==1){
+   count++;
+  } else if(x==0) {
+   count --;
+  }
+
+  setImage();
+
+}
+
+function setImage () {
+
+const WhichImg = "https://raw.githubusercontent.com/Jeevasimba/files/main/questiontagsverbs/"+count+".png";
+
+document.getElementById('soundtrack').innerHTML=`
+  <img src='${WhichImg}' alt="image" width=auto height=auto>
+`;
+
+}
